5ce1dc7274590ad82ef3cd45adafc63cd447c38f
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2023-06-22 11:21:24 +0700

    jika FRONT_position <= REAR_position, iterasi dari FRONT hingga REAR

--- CircularQueues/CircularQueues.cpp
+++ CircularQueues/CircularQueues.cpp
@@ -59,6 +59,7 @@ public:
 				FRONT = FRONT + 1;
 		}
 	}
+
 	void display() {
 		int FRONT_position = FRONT;
 		int REAR_position = REAR;
@@ -68,6 +69,16 @@ public:
 			cout << "Queue is empty\n";
 			return;
 		}
+
 		cout << "\nElements in the queue are: \n";
+
+		// jika FRONT_position <= REAR_position, iterasi dari FRONT hingga REAR
+		if (FRONT_position <= REAR_position) {
+			while (FRONT_position <= REAR_position) {
+				cout << queue_array[FRONT_position] << "  ";
+				FRONT_position++;
+			}
+			cout << endl;
+		}
 	}
 }; 
